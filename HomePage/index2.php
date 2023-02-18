74c8d9ddff47da82acf523b71e95b4d025bd0af4
Oneth Kahandawa <68096370+Onethk@users.noreply.github.com>
2023-02-18 21:23:51 +0530

    Add files via upload
    Home page first commit

---
+++ HomePage/index2.php
@@ -0,0 +1,69 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+
+    <section class="team">
+
+    <div class="container py-5">
+         <div class="row">
+            <div class="col-md-4">
+                <div class="card border-0 bg-light">
+                    <div class="card-body">
+                        <img src="images/oneth.jpeg" alt="" class="img-fluid" width="250px" height="250px">
+                    </div>
+                </div>
+                <h6>Oneth Kahandawa</h6>
+            </div>
+            <div class="col-md-4">
+                <div class="card border-0 bg-light">
+                    <div class="card-body">
+                    <img src="images/oneth.jpeg" alt="" class="img-fluid" width="250px" height="250px">
+                    </div>
+                </div> 
+                <h6>Oneth Kahandawa</h6>
+            </div>
+            <div class="col-md-4">
+                <div class="card border-0 bg-light">
+                    <div class="card-body">
+                    <img src="images/oneth.jpeg" alt="" class="img-fluid" width="250px" height="250px">
+                    </div>
+                </div>
+                <h6>Oneth Kahandawa</h6>
+            </div>
+            <div class="col-md-4">
+                <div class="card border-0 bg-light">
+                    <div class="card-body">
+                    <img src="images/oneth.jpeg" alt="" class="img-fluid" width="250px" height="250px">
+                    </div>
+                </div>
+                <h6>Oneth Kahandawa</h6>
+            </div>
+            <div class="col-md-4">
+                <div class="card border-0 bg-light">
+                    <div class="card-body">
+                    <img src="images/oneth.jpeg" alt="" class="img-fluid" width="250px" height="250px">
+                    </div>
+                </div>
+                <h6>Oneth Kahandawa</h6>
+            </div>
+         </div>
+         
+    </div>
+
+
+    </section>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+  </body>
+</html>
