8b8acc16fae56b55c698b8e34daeb193822c86fa
Oneth <onethkahandawa@gmail.com>
2023-02-23 14:23:41 +0530

    Commiting everything to login branch

---
+++ homePage.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+    <link rel="stylesheet" href="homePage.css">
+    
+  </head>
+
+  <body>
+
+
+    <nav class="navbar navbar-expand-lg">
+        <div class="container-fluid">
+        <a class="navbar-brand" href="#">Student Progress Pro</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                        <li class="nav-item">
+                        <a class="nav-link" href="#">Questions</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="tips.php">Tips n Tricks</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="#">Testomonials</a>
+                        </li>
+                        
+                        
+                    </ul>
+            </div>
+        </div>
+    </nav>
+
+    <section class="main float-right">
+        <div class="container py-9">
+            <div class="row py-1">
+                <div class="col-lg-7 pt-7 text-left">
+                    <h1 class="position-relative top-50 start-0 pt-1" style="font-size:4.5vw;">Thinking of how<br> to succeed, <br>we got you there !</h1>
+                </div>
+            </div>
+
+        </div>
+
+    </section>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+
+  </body>
+</html>     
