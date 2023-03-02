d48e0a60d7a0ebca38cf372a123229895ebf2b69
Dilukshan Thurairaj <thurairajdilu123@gmail.com>
2023-03-02 13:04:59 +0530

    Flash message implemented: After user's feedback is inserted to database a flash message is displayed

--- testo_backend/testo/testo.py
+++ testo_backend/testo/testo.py
@@ -1,8 +1,13 @@
-from flask import Flask, render_template, request, redirect, url_for
+import datetime as datetime
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
+# secret key given to display the flash
+app.secret_key = "Feedback message"
+
+# database
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
@@ -17,13 +22,21 @@ def index():
     return render_template('testo.html')
 
 
+# Insert the User's name, feedback and date into database
 @app.route('/insert', methods=['POST'])
 def insert():
     if request.method == "POST":
+        # Flashing a message
+        flash("Your feedback is successfully stored, Thank you for the feedback :)")
+
+        first_name = request.form['fName']
+        surname = request.form['sName']
         feedback = request.form['feedback']
+        date = datetime.date.today()
 
         cur = mysql.connection.cursor()
-        cur.execute("INSERT INTO testimonials (feedback) VALUES (%s)", (feedback,))
+        cur.execute("INSERT INTO testimonials (firstname, surname, feedback, date ) VALUES (%s, %s, %s, %s)",
+                    (first_name, surname, feedback, date,))
         mysql.connection.commit()
         return redirect(url_for('index'))
 
