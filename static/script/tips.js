a7feae07a56d7dd4ea686603cfac21265be7a4ff
kamil_thenuwara <kamil.20210331@iit.ac.lk>
2023-03-06 00:15:17 +0530

    declare tips

--- static/script/tips.js
+++ static/script/tips.js
@@ -10,3 +10,16 @@ if(predOut=="0 to 39"){
     tipMarkRange=69;
 }else(predOut=="70 to 79" || predOut=="80 to 89" || predOut=="90 to 100")
     tipMarkRange=100;
+
+    
+var tip1 ="if mark range is less than 40";
+var tip2 ="if mark range is less than 70";
+var tip3 ="if mark range is more than 70";
+var tip4 ="if study hours less than 2 hours";
+var tip5 ="if study hours less than 1 hours";
+var tip6 ="if attendance for lecs is poor";
+var tip7 ="if concentration in lecs is poor";
+var tip8 ="if spend more than 2 hours in social media";
+var tip9 ="if spend too much time on extra curricular acts";
+var tip10 ="if spare too much time on relationships";
+var tip11 ="if no friends to get help on studies";
